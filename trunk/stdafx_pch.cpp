32f02bd3bfe963cd5b6c9fa9dcde6ad06f90af69
liuyao0713 <liuyao0713@6b569256-3e9b-11de-b83a-779c162a6e82>
2009-07-23 16:31:04 +0000

    Changing design. Prepare discarding ugly DXUT code.

---
+++ trunk/stdafx_pch.cpp
@@ -0,0 +1,6 @@
+/**
+/file Precompiled header .cpp file
+The compiler uses this file to generate the precompiled header (.pch) file. 
+*/
+
+#include "stdafx_pch.h"
---
+++ trunk/stdafx_pch.h
@@ -0,0 +1,10 @@
+/**
+/file Precompiled header .h file
+Include core header files in this file to optimize compiler performance.
+*/
+
+#define _PRECOMPILED_HEADER_
+
+#include <stdio.h>
+#include <windows.h>
+#include <assert.h>
